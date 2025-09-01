ff49630dc58ab79754d9ab3f1f1fc16368adf547
Nicholas O'Neill <159676183+nicholasoneill97@users.noreply.github.com>
2025-09-01 15:58:53 -0400

    Update TripCards.js

--- src/app/components/TripCards.js
+++ src/app/components/TripCards.js
@@ -27,7 +27,7 @@ export default function TripCards() {
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true, amount: 0.2 }}
         transition={{ duration: 0.8 }}
-        className="mb-2"
+        className="mb-2 text-black"
       >
         We picked out some of the best trips that are looking extra special.
       </motion.p>
