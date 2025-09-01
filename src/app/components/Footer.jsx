54c2918a3ac296f506fc9e2fc0417bda03cf4a2f
Nicholas O'Neill <159676183+nicholasoneill97@users.noreply.github.com>
2025-09-01 16:49:58 -0400

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer className="mt-auto flex flex-col sm:flex-row justify-center items-center text-xs sm:text-sm gap-8 sm:gap-0 w-full bg-[#ddd] text-black p-6 sm:p-10">
+    <footer className="mt-auto flex flex-col sm:flex-row justify-center sm:justify-around items-center text-xs sm:text-sm gap-8 sm:gap-0 w-full bg-[#ddd] text-black p-6 sm:p-10">
         <ul className="flex flex-col gap-2 min-w-[100px] sm:min-w-[120px]">
           <li className="text-xl sm:text-2xl text-blue-500 m-0 p-0">
             Inter<span className="text-blue-800 m-0">Vest</span>
